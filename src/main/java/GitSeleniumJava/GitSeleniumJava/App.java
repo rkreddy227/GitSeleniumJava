a29573696e8cb2bf103d0bba34b0a8f5c9786858
rkreddy227 <rkreddy227@gmail.com>
2020-08-10 19:04:35 -0400

    First commit changes

---
+++ src/main/java/GitSeleniumJava/GitSeleniumJava/App.java
@@ -0,0 +1,13 @@
+package GitSeleniumJava.GitSeleniumJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
